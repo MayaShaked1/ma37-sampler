33a7564b569b5a1cd502218d2234d14ef1b5d452
MayaShaked1 <shakedmaya1@gmail.com>
2022-01-17 17:46:13 +0200

    updating jsonLoader

--- src/main/java/workspace/netanya/sampler/FileLoader/FileLoaderETL.java
+++ src/main/java/workspace/netanya/sampler/FileLoader/FileLoaderETL.java
@@ -1,5 +1,10 @@
 package workspace.netanya.sampler.FileLoader;
 
+import workspace.netanya.sampler.dataType.MadaReports;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public abstract class FileLoaderETL {
-    public abstract void FileLoaderETL();
+    public abstract void FileLoaderETL(ArrayList<ArrayList<MadaReports>> arrayListToLoad) throws IOException;
 }
--- src/main/java/workspace/netanya/sampler/FileLoader/JsonLoader.java
+++ src/main/java/workspace/netanya/sampler/FileLoader/JsonLoader.java
@@ -1,4 +1,25 @@
 package workspace.netanya.sampler.FileLoader;
 
-public class JsonLoader {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import workspace.netanya.sampler.dataType.MadaReports;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JsonLoader extends FileLoaderETL{
+
+    @Override
+    public void FileLoaderETL(ArrayList<ArrayList<MadaReports>> arrayListToLoad) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        for(int i=0;i<arrayListToLoad.size();i++)
+        {
+            String element="/file"+(i+1)+".json";
+
+            objectMapper.writeValue(new File("target/car.json"),arrayListToLoad.get(i));
+        }
+
+
+
+    }
 }
